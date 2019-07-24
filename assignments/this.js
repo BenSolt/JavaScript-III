a132131d6dd7d82e26cb51b09ee9d13ff7ae04fe
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-07-24 18:36:06 -0400

    finished. that was hard, but Fun!

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,93 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ */
+
+
+//1. Window Binding - is the entire javascript or the global.
+
+
+///////////////////////////////////////////////
+
+
+//2. Implicit Binding - points to object on which functionn called, (left of the.)
+
+
+///////////////////////////////////////////////////
+
+
+//3. New Binding - use new keyword, creates object and 'this points to it
+
+
+////////////////////////////////////////////////////////////////
+
+
+//4. Explicit Binding - tells javascript to set 'this' to point to certain value, using call, apply or bind
+
+
+///////////////////////////////////////////////////////////////////
+
+
+//write out a code example of each explanation above
+
 
 // Principle 1
 
 // code example for Window Binding
 
+
+function hello() {
+    console.log(this.greet);
+}
+
+const greet = "helllo there";
+
+hello();
+console.log(this);
+
+
+
+
 // Principle 2
 
 // code example for Implicit Binding
 
-// Principle 3
+
+let mydog = {
+    name: 'Kaylee',
+    sound: '2 wof wof',
+    speak: function() {
+        console.log(this.sound);
+    }
+}
+mydog.speak();
+
+
+
+
+//Principle 3
 
 // code example for New Binding
 
+
+function Doggy(saying) {
+    this.fact = saying;
+}
+let mydogb = new Doggy('3 Kaylee is all bark, no bite');
+console.log(mydogb.fact);
+
+
+
+
+
 // Principle 4
 
-// code example for Explicit Binding
+//code example for Explicit Binding
+
+function animal() {
+    console.log(this.speak);
+}
+const dog = {
+    name: 'Kaylee',
+    speak: '4 woooooooof',
+}
+animal.call(dog);
